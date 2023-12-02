ff335ec5f3fea3b340768deb494ddb7b5ead2503
maximsakovich <maximsakovich@gmail.com>
2023-12-02 16:19:13 +0100

    + update ConsoleMenu

--- CurrencyExchange/src/view/ConsoleMenu.java
+++ CurrencyExchange/src/view/ConsoleMenu.java
@@ -1,8 +1,14 @@
 package view;
 
 import interfaces.IConsoleMenu;
+import interfaces.ICurrencyRateModel;
+import interfaces.ITransactionModel;
+import interfaces.IUserModel;
+import model.CurrencyRateModel;
 import service.*;
 
+import java.util.Date;
+import java.util.List;
 import java.util.Scanner;
 
 public class ConsoleMenu implements IConsoleMenu {
@@ -123,28 +129,32 @@ public class ConsoleMenu implements IConsoleMenu {
         scanner.nextLine();
         switch (choice) {
             case 1://Просмотр профиля
-                viewProfile(userId);
+                handleViewUserProfile();
                 break;
             case 2://Просмотр баланса
                 viewBalance(userId);
                 break;
             case 3://Пополнение счета
-                accountService.deposit(userId);
+                handleDeposit();
                 break;
             case 4://Снятие средств
-                accountService.withdraw(userId);
+                handleWithdrawal();
                 break;
             case 5://Открытие нового счета
-                accountService.createAccount(userId);
+                handleCreateAccount();
                 break;
             case 6://Закрытие счета
-               accountService.closeAccount(userId);
+                handleCloseAccount();
                 break;
             case 7://Просмотр истории операций
-                viewTransactionHistory(userId);
+                handleViewTransactionHistory(currentUser.getUserId());
                 break;
+
+            default:
+                System.out.println("Неверный выбор. Пожалуйста, попробуйте снова.");
+                showUserMenu(currentUser.getUserId());
             case 8://Обмен валют
-               // transactionService.exchangeCurrency();
+                handleCurrencyExchange();
                 break;
             case 9://Выйти из системы
                 showMainMenu();
@@ -161,8 +171,13 @@ public class ConsoleMenu implements IConsoleMenu {
         scanner.nextLine();
         switch (choice) {
             case 1://Изменение курса валюты
-                changeCurrencyRate();
+                handleUpdateCurrencyRate();
                 break;
+
+            default:
+                System.out.println("Неверный выбор. Пожалуйста, попробуйте снова.");
+                showAdminMenu(userId);
+
             case 2://Добавление/удаление валюты
                 addOrRemoveCurrency();
                 break;
@@ -194,6 +209,7 @@ public class ConsoleMenu implements IConsoleMenu {
                 break;
             case 2:
                 System.out.println("Совершить транзакцию");
+                handleTransactionMenu();
                 break;
             case 3:
                 showUserMenu(123); // Заменить 123 на фактический идентификатор пользователя
@@ -203,4 +219,163 @@ public class ConsoleMenu implements IConsoleMenu {
                 showAccountMenu(456); // Заменить 456 на фактический идентификатор аккаунта
         }
     }
+
+    // Метод для обработки ввода в меню транзакции
+    private void handleTransactionMenu() {
+        System.out.println("Выберите тип транзакции:");
+        System.out.println("1. Пополнение счета");
+        System.out.println("2. Снятие средств");
+        System.out.println("3. Обмен валюты");
+
+        int transactionChoice = scanner.nextInt();
+        scanner.nextLine();
+
+        switch (transactionChoice) {
+            case 1:
+                // Пополнения счета
+                handleDeposit();
+                break;
+            case 2:
+                // Снятия средств
+                handleWithdrawal();
+                break;
+            case 3:
+                // Обмена валюты
+                handleCurrencyExchange();
+                break;
+            default:
+                System.out.println("Неверный выбор. Пожалуйста, попробуйте снова.");
+        }
+    }
+
+    // Метод для обработки просмотра профиля клиента
+    private void handleViewUserProfile() {
+        IUserModel user = userService.getUserById(currentUser.getUserId());
+        if (user != null) {
+            System.out.println("Информация о клиенте:");
+            System.out.println("Имя: " + user.getName());
+            System.out.println("Email: " + user.getEmail());
+
+        } else {
+            System.out.println("Пользователь не найден.");
+        }
+
+        // После просмотра профиля, показываем снова меню пользователя
+        showUserMenu(currentUser.getUserId());
+    }
+
+
+
+
+    // Метод для обработки пополнения депозита
+    private void handleDeposit() {
+        System.out.println("Введите идентификатор счета для пополнения депозита:");
+        int accountId = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Введите сумму для пополнения депозита:");
+        double amount = scanner.nextDouble();
+        scanner.nextLine();
+
+        // Вызов метода пополнения депозита
+        accountService.deposit(accountId, amount);
+    }
+
+    // Метод для обработки снятия средств
+    private void handleWithdrawal() {
+        System.out.println("Введите идентификатор счета для снятия средств:");
+        int accountId = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Введите сумму для снятия средств:");
+        double amount = scanner.nextDouble();
+        scanner.nextLine();
+
+        // Вызов метода снятия средств
+        accountService.withdraw(accountId, amount);
+    }
+
+    // Метод для обработки обмена валюты
+    private void handleCurrencyExchange() {
+        System.out.println("Введите идентификатор счета, с которого вы хотите обменять валюту:");
+        int accountIdFrom = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Введите идентификатор счета, на который вы хотите обменять валюту:");
+        int accountIdTo = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Введите сумму для обмена:");
+        double amount = scanner.nextDouble();
+        scanner.nextLine();
+
+        // Вызов метода обмена валюты
+        transactionService.exchangeCurrency(accountIdFrom, accountIdTo, amount);
+    }
+
+    // Метод для обработки открытия нового счета
+    private void handleCreateAccount() {
+        System.out.println("Выберите валюту для нового счета:");
+        // нужно добавить логику для выбора валюты, например, считывание кода валюты или ее названия
+
+        // вызов метода открытия нового счета
+        accountService.createAccount(userId, selectedCurrency); // Замените selectedCurrency на выбранную валюту
+    }
+    // Метод для обработки закрытия счета
+    private void handleCloseAccount() {
+        System.out.println("Введите идентификатор счета для закрытия:");
+        int accountIdToClose = scanner.nextInt();
+
+        // вызов метода закрытия счета
+        accountService.closeAccount(accountIdToClose);
+    }
+
+    // Метод для обработки просмотра истории операций
+    private void handleViewTransactionHistory(int userId) {
+        List<ITransactionModel> transactions = transactionService.getTransactionHistory(userId);
+        if (transactions != null && !transactions.isEmpty()) {
+            System.out.println("История операций:");
+
+            for (ITransactionModel transaction : transactions) {
+                System.out.println("Тип операции: " + transaction.getType());
+                System.out.println("Сумма: " + transaction.getAmount());
+                System.out.println("Дата: " + transaction.getTransactionDate());
+                System.out.println("-----------------------------");
+            }
+
+        } else {
+            System.out.println("Нет доступных операций для просмотра.");
+        }
+
+        // После просмотра истории операций, показываем снова меню пользователя
+        showUserMenu(userId);
+    }
+
+    // Метод для обработки изменения курса валюты
+    private void handleUpdateCurrencyRate() {
+        // Введите код валюты, для которой нужно изменить курс
+        System.out.println("Введите код валюты (например, USD): ");
+        String currencyCode = scanner.next();
+
+        // Получите текущий курс валюты
+        ICurrencyRateModel currentRate = currencyService.getCurrencyRate(currencyCode);
+
+        if (currentRate != null) {
+            // Запросите новый курс у пользователя
+            System.out.println("Текущий курс валюты " + currencyCode + ": " + currentRate.getRate());
+            System.out.println("Введите новый курс валюты: ");
+            double newRate = scanner.nextDouble();
+
+            // Создайте новую модель курса и обновите его
+            ICurrencyRateModel newCurrencyRate = new CurrencyRateModel(currencyCode, newRate, new Date());
+            currencyService.updateCurrencyRate(newCurrencyRate);
+        } else {
+            System.out.println("Курс валюты с кодом " + currencyCode + " не найден.");
+        }
+
+        // После обновления курса валюты, показываем снова меню администратора
+        showAdminMenu();
+    }
+
+
 }
