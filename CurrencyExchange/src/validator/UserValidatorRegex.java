b4e6b300ceb67be74f973d17d6d5056904c693a0
Yevhen Kim <evgeniikim@gmail.com>
2023-12-04 21:56:14 +0100

    + output console comments

--- CurrencyExchange/src/validator/UserValidatorRegex.java
+++ CurrencyExchange/src/validator/UserValidatorRegex.java
@@ -14,18 +14,23 @@ public class UserValidatorRegex implements IUserValidator {
 
     @Override
     public boolean validateEmail(String email) {
-        return email != null && Pattern.matches(EMAIL_REGEX, email);
+        var result = email != null && Pattern.matches(EMAIL_REGEX, email);
+        System.out.println("Email не соответствует формату!");
+        return result;
     }
 
     @Override
     public boolean validatePassword(String password) {
         if (password == null || password.length() < MIN_PASSWORD_LENGTH) {
+            System.out.println("Password не соответствует формату!");
             return false;
         }
         if (REQUIRE_SPECIAL_CHAR && !password.matches(".*[!@#$%^&*()].*")) {
+            System.out.println("Password должен содержать спец символы!");
             return false;
         }
         if (REQUIRE_DIGITS && !password.matches(".*\\d.*")) {
+            System.out.println("Password должен содержать цифры!");
             return false;
         }
         return true;
