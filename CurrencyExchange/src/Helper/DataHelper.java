75dacac6b2bbefa4ff22fc23dc9845aa5eb42027
Yevhen Kim <evgeniikim@gmail.com>
2023-12-05 18:55:45 +0100

    + json serialize variant

--- CurrencyExchange/src/Helper/DataHelper.java
+++ CurrencyExchange/src/Helper/DataHelper.java
@@ -2,6 +2,7 @@ package Helper;
 
 import java.io.*;
 import java.util.Map;
+import com.google.gson.Gson;
 
 public class DataHelper {
 
@@ -21,6 +22,27 @@ public class DataHelper {
         }
         else {
             System.out.println("Файл не существует "+fileName);
+            return null;
+        }
+    }
+
+    private static final Gson gson = new Gson();
+
+    public static void exportDataToJson(String fileName, Object data) throws IOException {
+        try (Writer writer = new FileWriter(fileName)) {
+            gson.toJson(data, writer);
+        }
+    }
+
+    public static <T> T importDataFromJson(String fileName, Class<T> type) throws IOException {
+        File file = new File(fileName);
+        if (file.exists()) {
+            System.out.println("Файл загружен " + fileName);
+            try (Reader reader = new FileReader(fileName)) {
+                return gson.fromJson(reader, type);
+            }
+        } else {
+            System.out.println("Файл не существует " + fileName);
             return null;
         }
     }
