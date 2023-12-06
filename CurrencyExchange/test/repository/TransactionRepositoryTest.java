584c8608e9fbf92ff70c21a5f0fc97b228936236
Yevhen Kim <evgeniikim@gmail.com>
2023-12-06 02:37:26 +0100

    ! test for TransactionRepository

--- CurrencyExchange/test/repository/TransactionRepositoryTest.java
+++ CurrencyExchange/test/repository/TransactionRepositoryTest.java
@@ -1,103 +1,47 @@
 package repository;
 
-import interfaces.ITransactionModel;
-import interfaces.TransactionType;
-import model.TransactionModel;
+import model.*;
+import interfaces.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.Date;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class TransactionRepositoryTest {
-
-    private static final String USER_TRANSACTIONS_FILE = "usertransactions_test.dat";
-
-    private TransactionRepository repository;
+    private TransactionRepository transactionRepository;
+    private AccountRepository accountRepository;
+    private TransactionModel transaction1;
+    private TransactionModel transaction2;
 
     @BeforeEach
     void setUp() {
-        repository = new TransactionRepository();
+        accountRepository = new AccountRepository();
+        transactionRepository = new TransactionRepository(accountRepository);
+
+        // Добавляем тестовые аккаунты
+        accountRepository.saveAccount(new AccountModel(1, 1, new CurrencyModel("USD", "Dollar"), 100.0));
+        accountRepository.saveAccount(new AccountModel(2, 1, new CurrencyModel("EUR", "Euro"), 200.0));
+
+        // Создаем и добавляем тестовые транзакции
+        transaction1 = new TransactionModel(1, 1, TransactionType.DEPOSIT, 100.0, new Date());
+        transaction2 = new TransactionModel(2, 2, TransactionType.WITHDRAWAL, 50.0, new Date());
+        transactionRepository.saveTransaction(transaction1);
+        transactionRepository.saveTransaction(transaction2);
     }
 
     @Test
-    void saveTransaction() {
-        ITransactionModel transaction = new TransactionModel(1, 2, TransactionType.WITHDRAWAL, 200, new Date(2023, 12, 3));
-
-        repository.saveTransaction(transaction);
-
-        List<ITransactionModel> transactions = repository.findTransactionsByUserId(1);
-
+    void saveTransactionAndFindTransactionsByUserId() {
+        List<ITransactionModel> foundTransactions = transactionRepository.findTransactionsByUserId(1);
+        assertTrue(foundTransactions.contains(transaction1) && foundTransactions.contains(transaction2),
+                "Должны быть найдены все транзакции пользователя");
     }
 
     @Test
-    void findTransactionsByUserId() {
-        ITransactionModel transaction1 = new TransactionModel(1, 1, TransactionType.DEPOSIT, 4000, new Date(2023, 12, 2));
-        ITransactionModel transaction2 = new TransactionModel(1, 2, TransactionType.DEPOSIT, 6000, new Date(2023, 12, 2));
-
-        repository.saveTransaction(transaction1);
-        repository.saveTransaction(transaction2);
-
-        List<ITransactionModel> transactions = repository.findTransactionsByUserId(1);
-
-        assertTrue(transactions.contains(transaction1));
-
-    }
-
-    @Test
-    void getNextTransactionId() {
-        ITransactionModel transaction1 = new TransactionModel(1, 1, TransactionType.DEPOSIT, 3000, new Date(2023, 12, 3));
-        ITransactionModel transaction2 = new TransactionModel(2, 2, TransactionType.DEPOSIT, 4000, new Date(2023, 12, 3));
-
-        repository.saveTransaction(transaction1);
-        repository.saveTransaction(transaction2);
-        assertEquals(3, repository.getNextTransactionId());
-    }
-
-    @Test
-    void saveToFile() {
-        ITransactionModel transaction1 = new TransactionModel(1, 1, TransactionType.DEPOSIT, 2500, new Date(2023, 12, 3));
-        ITransactionModel transaction2 = new TransactionModel(2, 2, TransactionType.DEPOSIT, 3200, new Date(2023, 12, 3));
-
-        repository.saveTransaction(transaction1);
-        repository.saveTransaction(transaction2);
-
-        int result = repository.saveToFile();
-        assertEquals(0, result);
-    }
-
-    @Test
-    void loadFromFile() {
-        ITransactionModel transaction1 = new TransactionModel(1, 1, TransactionType.DEPOSIT, 5000, new Date(2023, 12, 3));
-        ITransactionModel transaction2 = new TransactionModel(2, 2, TransactionType.DEPOSIT, 5200, new Date(2023, 12, 3));
-
-        repository.saveTransaction(transaction1);
-        repository.saveTransaction(transaction2);
-        repository.saveToFile();
-
-        repository = new TransactionRepository();
-
-        int result = repository.loadFromFile();
-
-        assertEquals(0, result);
-
-        List<ITransactionModel> transactions = repository.findTransactionsByUserId(1);
-
-    }
-
-    @Test
-    void getAllTransactions() {
-        ITransactionModel transaction1 = new TransactionModel(1, 1, TransactionType.DEPOSIT, 1500, new Date(2023, 12, 3));
-        ITransactionModel transaction2 = new TransactionModel(2, 2, TransactionType.DEPOSIT, 2200, new Date(2023, 12, 3));
-
-        repository.saveTransaction(transaction1);
-        repository.saveTransaction(transaction2);
-
-        List<ITransactionModel> allTransactions = repository.getAllTransactions();
-
-        assertTrue(allTransactions.contains(transaction1));
-        assertTrue(allTransactions.contains(transaction2));
+    void findTransactionsByAccountId() {
+        List<ITransactionModel> transactions = transactionRepository.findTransactionsByAccountId(1);
+        assertTrue(transactions.contains(transaction1), "Должна быть найдена транзакция по ID аккаунта");
     }
 }
+
